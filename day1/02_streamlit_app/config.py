979010964fa2d99c682669d5d05293f9e0291302
Y <159997300+isdlyusukeshimizu@users.noreply.github.com>
2025-04-23 16:13:05 +0900

    Update config.py

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -1,3 +1,5 @@
 # config.py
 DB_FILE = "chat_feedback.db"
-MODEL_NAME = "google/gemma-2-2b-jpn-it"
+MODEL_NAME = "google/gemma-7b-it"
+# MODEL_NAME = "google/gemma-1.1-2b-it"
+# MODEL_NAME = "google/gemma-2-2b-jpn-it"
