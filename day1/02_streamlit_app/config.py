1f732ab2f3be959fe816b68ee3762e6b63660ff7
Y <159997300+isdlyusukeshimizu@users.noreply.github.com>
2025-04-23 16:16:43 +0900

    Update config.py

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -3,3 +3,8 @@ DB_FILE = "chat_feedback.db"
 MODEL_NAME = "google/gemma-7b-it"
 # MODEL_NAME = "google/gemma-1.1-2b-it"
 # MODEL_NAME = "google/gemma-2-2b-jpn-it"
+
+# 感想および考察
+# gemma-7b-itはVRAMが必要と他の方の技術ブログに記載があったが、推論速度はあまり変わらなかった。
+# 上記の考察として、Google Colab ProのA100/GPUを使っていたため、7Bモデルでも余裕で処理できる性能だったので、
+# 2Bとあまり差が出なかったと考える。また、入力トークン数をかなり増やすことでパフォーマンスの変化を感じられた可能性もある。
